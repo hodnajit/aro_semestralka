3792e947b61771e8c0a44991784c4cc628fc9346
Tomáš Rouček <rouceto1@fel.cvut.cz>
2019-05-08 12:35:24 +0200

    barbie clustering

--- src/barbie_detection/scripts/barbie_selection.py
+++ src/barbie_detection/scripts/barbie_selection.py
@@ -9,16 +9,36 @@ from exploration.srv import PlanPath,  PlanPathRequest, PlanPathResponse
 import numpy as np
 import tf.transformations as tft
 import tf2_ros
+import math 
 
-
+tresh = 0.2
 detections = []
+def getDist(pose1,pose2):
+    x1 = pose1.position.x
+    y1 = pose1.position.y
+    x2 = pose2.position.x
+    y2 = pose2.position.y
+    return math.hypot(x2 - x1, y2 - y1)
+
 def barbie_cb(msg):
 
     detections.append(msg.point)
+    counter = []
+    for x in xrange(detections):
+        counter.append(0)
+    for index,det in enumerate(detections):
+        for nn in detections:
+            if getDist(det,nn) < tresh :
+                counter[index] +=1
+
 
     header = Header(stamp=rospy.Time.now(), frame_id="camera_rgb_optical_frame")
-    msg = Marker(header=header, pose=Pose(position=msg.point), id=np.random.randint(0, 1e9), type=Marker.SPHERE, scale=Vector3(0.05, 0.05, 0.05), color=ColorRGBA(0, 0, 1, 1), lifetime=rospy.Duration(0))
+    msg = Marker(header=header, pose=Pose(position=msg.point), id=np.random.randint(0, 1e9), type=Marker.SPHERE, scale=Vector3(0.01, 0.01, 0.01), color=ColorRGBA(0, 0, 1, 1), lifetime=rospy.Duration(0))
     markerAllPublisher.publish(msg)
+    best = detections[counter.index(max(counter))]#find index of largest
+
+    msg = Marker(header=header, pose=Pose(position=best), id=np.random.randint(0, 1e9), type=Marker.SPHERE, scale=Vector3(0.05, 0.05, 0.05), color=ColorRGBA(1, 0, 0, 1), lifetime=rospy.Duration(0))
+    markerPublisher.publish(msg)
 
 
 if __name__ == "__main__":
