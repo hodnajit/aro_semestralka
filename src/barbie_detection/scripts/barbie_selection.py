b347808c6e031b82aa6e1eeea707ba4a982ace8f
Posranej Turtlebot <posranejturtlebot@udementa.cz>
2019-05-08 14:00:23 +0200

    Filtration working

--- src/barbie_detection/scripts/barbie_selection.py
+++ src/barbie_detection/scripts/barbie_selection.py
@@ -4,8 +4,6 @@ from nav_msgs.msg import Path, OccupancyGrid
 from std_msgs.msg import Header, ColorRGBA
 from geometry_msgs.msg import PointStamped, Pose, Vector3
 from visualization_msgs.msg import Marker, MarkerArray
-from exploration.srv import AnyFrontiersLeft, AnyFrontiersLeftRequest, AnyFrontiersLeftResponse, GenerateFrontier, GenerateFrontierResponse
-from exploration.srv import PlanPath,  PlanPathRequest, PlanPathResponse
 import numpy as np
 import tf.transformations as tft
 import tf2_ros
@@ -14,18 +12,18 @@ import math
 tresh = 0.2
 detections = []
 def getDist(pose1,pose2):
-    x1 = pose1.position.x
-    y1 = pose1.position.y
-    x2 = pose2.position.x
-    y2 = pose2.position.y
+    x1 = pose1.x
+    y1 = pose1.y
+    x2 = pose2.x
+    y2 = pose2.y
     return math.hypot(x2 - x1, y2 - y1)
 
 def barbie_cb(msg):
 
     detections.append(msg.point)
     counter = []
-    for x in xrange(detections):
-        counter.append(0)
+    counter= [0] * len(detections)    
+
     for index,det in enumerate(detections):
         for nn in detections:
             if getDist(det,nn) < tresh :
@@ -33,11 +31,11 @@ def barbie_cb(msg):
 
 
     header = Header(stamp=rospy.Time.now(), frame_id="camera_rgb_optical_frame")
-    msg = Marker(header=header, pose=Pose(position=msg.point), id=np.random.randint(0, 1e9), type=Marker.SPHERE, scale=Vector3(0.01, 0.01, 0.01), color=ColorRGBA(0, 0, 1, 1), lifetime=rospy.Duration(0))
+    msg = Marker(header=header, pose=Pose(position=msg.point), id=np.random.randint(0, 1e9), type=Marker.SPHERE, scale=Vector3(0.04, 0.04, 0.04), color=ColorRGBA(1, 1, 1, 1), lifetime=rospy.Duration(0))
     markerAllPublisher.publish(msg)
     best = detections[counter.index(max(counter))]#find index of largest
 
-    msg = Marker(header=header, pose=Pose(position=best), id=np.random.randint(0, 1e9), type=Marker.SPHERE, scale=Vector3(0.05, 0.05, 0.05), color=ColorRGBA(1, 0, 0, 1), lifetime=rospy.Duration(0))
+    msg = Marker(header=header, pose=Pose(position=best), id=1, type=Marker.SPHERE, scale=Vector3(0.1, 0.1, 0.1), color=ColorRGBA(1, 0, 0, 1), lifetime=rospy.Duration(0))
     markerPublisher.publish(msg)
 
 
